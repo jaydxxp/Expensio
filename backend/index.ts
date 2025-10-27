a9d827f972e3fedcbb688911b958714640587e2a
jaydxxp <jaydeepwaghaskar2005@gmail.com>
2025-10-28 03:32:59 +0530

    minor cors error

--- backend/index.ts
+++ backend/index.ts
@@ -2,7 +2,7 @@ import express from "express"
 import cors from "cors"
 import Mainrouter from "./routes/route"
 const app=express();
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 app.use("/api/v1",Mainrouter);
 app.listen(3000);
